eb4aaf044f7ec30853934c04d262a4d28a59ff01
Nisa Karima <nissakarima@gmail.com>
2026-04-30 10:43:08 +0700

    delete agent

--- tests/Automate.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { runAgent } from '../agent/agent.js';
-
-test('Agentic QA Test', async ({ request }) => {
-  await runAgent(request);
-
-  expect(true).toBeTruthy();
-});
